28b5a3698aeae5a8005e6d47f351e54dc0afe5b3
shayan-mrafiee <shayan.mrafiee.chatgpt@gmail.com>
2025-05-22 00:56:51 -0400

    Renamed the directory and included helpful files.

---
+++ AtCoder/AtCoder Regular Contest 197 - Div. 2/AtCoder Regular Contest 197 - Div. 2/GridPaths.cpp
@@ -0,0 +1,16 @@
+//#include <bits/stdc++.h>
+//
+//using namespace std;
+//
+//void solve() {
+//    int H, W; string S;
+//    cin >> H >> W >> S;
+//    
+//    
+//}
+//
+//int main() {
+//    int T; cin >> T;
+//    while(T--)
+//        solve();
+//}
