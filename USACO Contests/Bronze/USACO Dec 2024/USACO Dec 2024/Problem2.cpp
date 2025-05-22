617495e54083adb85792132d5db682e2e38c5be0
shayan-mrafiee <shayan.mrafiee.chatgpt@gmail.com>
2025-05-22 01:08:23 -0400

    Edited some code to make them neater.

--- USACO Contests/Bronze/USACO Dec 2024/USACO Dec 2024/Problem2.cpp
+++ USACO Contests/Bronze/USACO Dec 2024/USACO Dec 2024/Problem2.cpp
@@ -1,28 +1,22 @@
-//#include <iostream>
-//#include <map>
-//#include <vector>
-//#include <algorithm>
-//
-//using namespace std;
-//
-//int main() {
-//    int N, Q; cin >> N >> Q;
-//    int blocks = 0;
-//    vector<int> v (N, 0);
-//    vector<vector<int>> Xs (N, v), Ys (N, v), Zs (N, v);
-//    
-//    for (int q = 0; q < Q; q++) {
-//        int x, y, z;
-//        cin >> x >> y >> z;
-//        Xs[y][z]++;
-//        blocks += (Xs[y][z] == N) ? 1 : 0;
-//        
-//        Ys[x][z]++;
-//        blocks += (Ys[x][z] == N) ? 1 : 0;
-//        
-//        Zs[x][y]++;
-//        blocks += (Zs[x][y] == N) ? 1 : 0;
-//        
-//        cout << blocks << endl; 
-//    }
-//}
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int N, Q; cin >> N >> Q;
+    int blocks = 0;
+    vector<vector<int>> Xs(N, vector<int>(N)), Ys, Zs;
+    Ys = Zs = Xs;
+    
+    while(Q--) {
+        int x, y, z;
+        cin >> x >> y >> z;
+        Xs[y][z]++;
+        Ys[x][z]++;
+        Zs[x][y]++;
+        
+        blocks += (Xs[y][z] == N) + (Ys[x][z] == N) + (Zs[x][y] == N);
+        
+        cout << blocks << "\n";
+    }
+}
