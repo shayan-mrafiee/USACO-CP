ed883553edfb6b4e256012e600cd4a09baf096c6
shayan-mrafiee <shayan.mrafiee.chatgpt@gmail.com>
2025-05-22 23:19:58 -0400

    Went over coffee.cpp code

--- Topics/Prefix Sums/Prefix Sums/coffee.cpp
+++ Topics/Prefix Sums/Prefix Sums/coffee.cpp
@@ -1,33 +1,29 @@
-//#include <bits/stdc++.h>
-//
-//using namespace std;
-//const int maxN = 200001;
-//using ll = long long;
-//
-//int main() {
-//    int n, k, q; cin >> n >> k >> q;
-//    ll dif[maxN+2] = {};
-//    
-//    for (int i = 0; i < n; i++) {
-//        int l, r; cin >> l >> r;
-//        dif[l]++; dif[r+1]--;
-//    }
-//    
-//    // convert difference array
-//    for (int i = 1; i <= maxN; i++) {
-//        dif[i] += dif[i-1];
-//    }
-//    
-//    // construct prefix array
-//    ll prefix[maxN+1] = {};
-//    for (int i = 1; i <= maxN; i++) {
-//        prefix[i] = prefix[i-1] + (dif[i] >= k);
-//    }
-//    
-//    // process queries
-//    while(q--) {
-//        int a, b;
-//        cin >> a >> b;
-//        cout << prefix[b] - prefix[a-1] << "\n";
-//    }
-//}
+#include <bits/stdc++.h>
+
+using namespace std;
+const int maxN = 200001;
+using ll = long long;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(nullptr);
+    
+    int n, k, q; cin >> n >> k >> q;
+    ll diff[maxN] = {}, prefix[maxN] = {};
+    
+    while(n--) {
+        int l, r; cin >> l >> r;
+        diff[l]++;
+        if (r < maxN-1) diff[r+1]--;
+    }
+    
+    for (int i = 1; i < maxN; i++) {
+        diff[i] += diff[i-1];
+        prefix[i] = prefix[i-1] + (diff[i] >= k);
+    }
+    
+    while(q--) {
+        int a, b; cin >> a >> b;
+        cout << prefix[b] - prefix[a-1] << "\n"; 
+    }
+}
