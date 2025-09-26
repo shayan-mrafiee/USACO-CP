5bbea0fae09c211a2ad4f495de3421c9cf864617
shayan-mrafiee <shayan.mrafiee.chatgpt@gmail.com>
2025-09-26 07:06:23 -0500

    working on it...

--- USACO & CP/USACO & CP/Topics/Functional Graphs/visits.cpp
+++ USACO & CP/USACO & CP/Topics/Functional Graphs/visits.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long; 
+using ll = long long;
 #define all(x) x.begin(), x.end()
 
 struct Cow {
@@ -9,53 +9,40 @@ struct Cow {
 };
 
 vector<Cow> cows;
-vector<pair<int, int>> cycle;
-vector<vector<int>> cycles;
-vector<ll> cycle_sum;
-ll max_moo = 0;
+vector<bool> visited;
+ll max_moos = 0;
+
+int min_moo_in_cycle(int u) {
+    int min_moo = cows[u].v;
+    int v = cows[u].a;
+    
+    while(v != u) {
+        min_moo = min(min_moo, cows[v].v);
+        v = cows[v].a;
+    }
+    
+    return min_moo;
+}
+
+void dfs(int u) {
+    visited[u] = true;
+    
+    max_moos += cows[u].v;
+    if (visited[cows[u].a]) {
+        max_moos -= min_moo_in_cycle(cows[u].a);
+    }
+    else dfs(cows[u].a);
+}
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(nullptr); cout.tie(nullptr);
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
     
     int N; cin >> N;
     cows.resize(N);
-    cycle = vector<pair<int, int>>(N, {-2, -2});
+    visited.resize(N);
     
-    for (auto &[a, v] : cows) {
-        cin >> a >> v;
-        a--;
-    }
+    for (auto &[a, v] : cows) cin >> a >> v;
     
-    ll cur_moo = 0;
-    stack<int> s;
-    for (int u = 0; u < N; u++) {
-        if (cycle[u].first > -1) {
-            int n = cycles[cycle[u].first].size(),
-            i = cycle[u].first, j = cycle[u].second;
-            cur_moo += cycle_sum[i] - cycles[i][(j + n-1) % n];
-            max_moo = max(max_moo, cur_moo);
-            cur_moo = 0;
-        }
-        
-        else if (cycle[u].first == -1) {
-            cycles.push_back({});
-            cycle_sum.push_back(0);
-            while(s.top() != u) {
-                
-            }
-            cycles.back().push_back(u);
-            cycle[u] = {cycles.size()-1, cycles.back().size()-1};
-            cycle_sum.back() += cows[u].v;
-            s.pop();
-        }
-        
-        else {
-            cycle[u].first = -1;
-            cur_moo += cows[u].v;
-            s.push(u);
-        }
-    }
     
-    cout << max_moo << "\n";
 }
