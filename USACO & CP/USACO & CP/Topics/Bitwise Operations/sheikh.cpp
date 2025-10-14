d3d6980832e45b503b950ff94432a15ac0e6919b
shayan-mrafiee <shayan.mrafiee88@gmail.com>
2025-10-14 11:29:19 -0500

    Update sheikh.cpp

--- USACO & CP/USACO & CP/Topics/Bitwise Operations/sheikh.cpp
+++ USACO & CP/USACO & CP/Topics/Bitwise Operations/sheikh.cpp
@@ -11,25 +11,32 @@ void solve() {
     for (int &i : a) cin >> i;
     int L, R; cin >> L >> R;
     
-    ll max_f = 0, cur_sum = 0, cur_xor = 0;
-    int l = 0, r = 0;
+    ll max_f = accumulate(a.begin() + L-1, a.begin() + R, 0LL) - 
+    accumulate(a.begin() + L-1, a.begin() + R, 0LL, [](ll a, ll b) {
+        return a ^ b; 
+    }),
     
-    for (int i = 0; i < n; i++) {
-        cur_sum += a[i];
-        cur_xor ^= a[i];
+    cur_sum = 0, cur_xor = 0; 
+    
+    int l = L-1, r = R-1; 
+    int last = L-1; 
+    for (int i = L-1; i < R; i++) {
+        cur_sum += a[i]; 
+        cur_xor ^= a[i]; 
         
-        if (cur_sum - cur_xor < a[i]) {
-            cur_sum = cur_xor = a[i];
-            l = i;
-        }
-        
-        if (cur_sum - cur_xor > max_f) {
-            max_f = cur_sum + cur_xor;
-            r = i;
+        if (cur_sum - cur_xor == max_f) {
+            for (int j = last; j <= i; j++) {
+                if (cur_sum - cur_xor == max_f && i - j < r - l) {
+                    r = i; l = j; 
+                } 
+                cur_sum -= a[j];  
+                cur_xor ^= a[j]; 
+            }
+            cur_sum = cur_xor = 0; 
         }
     }
     
-    cout << min(l+1, r+1) << " " << r+1 << "\n";
+    cout << l+1 << ' ' << r+1 << "\n"; 
 }
 
 int main() {
