feb7b1101beeca65e992de2ade505b18acd75aaf
shayan-mrafiee <shayan.mrafiee.chatgpt@gmail.com>
2026-02-15 13:56:44 -0600

    solved rotations.cpp

--- USACO & CP/USACO & CP/USACO Contests/Silver/2025-2026/Contest 2/rotations.cpp
+++ USACO & CP/USACO & CP/USACO Contests/Silver/2025-2026/Contest 2/rotations.cpp
@@ -4,83 +4,50 @@ using namespace std;
 using ll = long long;
 #define all(x) x.begin(), x.end()
 
+void solve(vector<int> &A, vector<int> &ans) {
+    int N = (int) A.size() / 2; 
+    int need = set<int>(A.begin(), A.end()).size();
+    vector<int> ocr(N+1);
+
+    // start going to the left, then go to the right
+    // cost would be i - l + r - l = i + (r - 2l)
+    // store the minimum (r - 2l) for each i
+    int best = INT32_MAX; // stores minimum (r - 2l)
+    int r = -1; 
+    for (int i = 0; i < 2*N; i++) {
+        while (need && r+1 < 2*N) {
+            r++;
+            need -= (++ocr[A[r]]) == 1; 
+        }
+
+        if (!need) best = min(best, r - 2*i); // set l to i and check 
+
+        ans[i] = min(ans[i], i + best); // here i is the index, not l
+
+        ocr[A[i]]--; 
+        need += ocr[A[i]] == 0;
+    }
+}
+
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
     
     int N; cin >> N; 
-    vector<int> A(3*N);
-    vector<vector<int>> indicies(N+1);
-    set<int> S1, S2; 
+    vector<int> A(2*N), ans(2*N, INT32_MAX);
 
-    for (int i = N; i < 2*N; i++) {
+    for (int i = 0; i < N; i++) {
         cin >> A[i];
-        A[i-N] = A[i+N] = A[i];
-        indicies[A[i]].push_back(i);
-        S1.insert(A[i]);
-        S2.insert(A[i]);
+        A[i+N] = A[i];
     }
 
-    vector<int> ans(N);
-    priority_queue<int, vector<int>, greater<int>> pq1; 
-    for (int i = N, l = N, r = N; i < 2*N; i++) {
-        S1.erase(A[i]);
-        while (!S1.empty() && l) {
-            l--; 
-            if (S1.count(A[l])) {
-                pq1.push(l);
-                S1.erase(A[l]);
-            }
-        }
-
-        r = max(r, i);
-        int cur_ans = 2*(r - i) + i - l;
-
-        int top = pq1.top(); pq1.pop(); 
-        int new_l = pq1.top(); 
-        int new_r = *lower_bound(all(indicies[A[top]]), i);
-        while (!pq1.empty() && 2 * max(new_r - i, r - i) + i - new_l < cur_ans) {
-            r = max(r, new_r);
-            l = new_l;
-            cur_ans = 2*(r - i) + i - l;
-            top = pq1.top(); pq1.pop(); 
-            new_l = pq1.top(); 
-            new_r = *lower_bound(all(indicies[A[top]]), i);
-        }
-        pq1.push(top);
-
-        ans[i - N] = cur_ans;
-    }
-    // do the same logic as the loop above, but reverse it (backward loop, have pq store r's not l's)
-    priority_queue<int> pq2; 
-    for (int i = 2*N-1, l = 2*N-1, r = 2*N-1; i >= N; i--) {
-        S2.erase(A[i]);
-        while (!S2.empty() && r < 3*N-1) {
-            r++; 
-            if (S2.count(A[r])) {
-                pq2.push(r);
-                S2.erase(A[r]);
-            }
-        }
-
-        l = min(l, i);
-        int cur_ans = 2*(i - l) + r - i;
-
-        int top = pq2.top(); pq2.pop(); 
-        int new_r = pq2.top(); 
-        int new_l = *(upper_bound(all(indicies[A[top]]), i) - 1);
-        while (!pq2.empty() && 2 * max(i - new_l, i - l) + new_r - i < cur_ans) {
-            l = min(l, new_l);
-            r = new_r;
-            cur_ans = 2*(i - l) + r - i;
-            top = pq2.top(); pq2.pop(); 
-            new_r = pq2.top(); 
-            new_l = *(upper_bound(all(indicies[A[top]]), i) - 1);
-        }
-        pq2.push(top);
-
-        ans[i - N] = min(cur_ans, ans[i - N]);
+    for (int i = 0; i < 2; i++) {
+        solve(A, ans);
+        reverse(all(A));
+        reverse(all(ans));
     }
 
-    for (int j : ans) cout << j << "\n";
+    for (int i = 0; i < N; i++) {
+        cout << min(ans[i], ans[i+N]) << " \n"[i == N-1]; 
+    }
 }
